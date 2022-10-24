e387451810c117d4ac4486087b2875adb34a8d43
Julia <jgrinja@gmail.com>
2022-10-24 12:49:24 +0200

    added nuxtServerInit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,27 +11,10 @@ export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(()=> {
-        resolve( {
-          loadedPosts: [
-            {id: "1", title: "Hello there!", previewText: "This my first post!", thumbnail:"https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"},
-            {id: "2", title: "Hello there - the second time!", previewText: 'This my second post!', thumbnail:"https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"},
-            {id: "3", title: "Hello!", previewText: "This my third post!", thumbnail:"https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"},
-          ]
-        });
-      }, 1000)
-    })
-      .then(data => {
-       return data
-      })
-      .catch(e => {
-      context.error(e);
-    });
-  },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts)
+  computed: {
+    loadedPosts(state) {
+      return this.$store.getters.loadedPosts
+    }
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -11,6 +11,33 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context){
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              vuexContext.commit('setPosts', [
+                {
+                  id: "1",
+                  title: "Hello there!",
+                  previewText: "This my first post!",
+                  thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+                },
+                {
+                  id: "2",
+                  title: "Hello there - the second time!",
+                  previewText: 'This my second post!',
+                  thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+                },
+                {
+                  id: "3",
+                  title: "Hello!",
+                  previewText: "This my third post!",
+                  thumbnail: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+                },
+              ])
+              resolve();
+            }, 1000)
+          });
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
       }
